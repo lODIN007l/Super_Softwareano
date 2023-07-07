53edb2c4b12943cbf1ccad59b01e85bcc89b4a4b
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2023-07-07 01:21:45 -0500

    envio capturas

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ class Game:
 	def __init__(self):
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-		pygame.display.set_caption('Western shooter')
+		pygame.display.set_caption('Super_sofwareanos')
 		self.clock = pygame.time.Clock()
 
 		# groups 
